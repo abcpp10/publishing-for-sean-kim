4afea0d9780833a5b9c1f7511874e97e471c421f
dggoo <98090836+dggoo@users.noreply.github.com>
2022-01-21 00:08:48 +0900

    Add files via upload

---
+++ mainlinks.js
@@ -0,0 +1,7 @@
+const letterbutton = document.querySelector("#letterButton");
+
+letterbutton.addEventListener('click',xGoLetter,false);
+
+function xGoLetter() {
+    location.href="letter.html";
+}
