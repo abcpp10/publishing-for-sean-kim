78be2fb1d0240698e26d1803762d3d37a5bbad4a
dggoo <98090836+dggoo@users.noreply.github.com>
2022-01-23 01:11:28 +0900

    Delete mainlinks.js

--- mainlinks.js
+++
@@ -1,7 +0,0 @@
-const letterbutton = document.querySelector("#letterButton");
-
-letterbutton.addEventListener('click',xGoLetter,false);
-
-function xGoLetter() {
-    location.href="letter.html";
-}
