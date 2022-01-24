9f66229baa4fb692bc848c6a3dae23efe61fb3ee
dggoo <98090836+dggoo@users.noreply.github.com>
2022-01-25 01:34:42 +0900

    Update lettersubmit.js

--- lettersubmit.js
+++ lettersubmit.js
@@ -1,11 +1,32 @@
-const lettersubmit = document.querySelector("#letterSubmit");
+const letterSubmit = document.querySelector("#letterSubmitButton");
+const letterField = document.querySelector("#letterField");
+const letterFieldErrorMessage = document.querySelector("#letterFieldErrorMessage");
 
-lettersubmit.addEventListener('click',xLetterSubmit,false);
-
+letterSubmit.addEventListener('click',xLetterSubmit,false);
+letterField.addEventListener('keydown',xLetterFieldNoError,false);
 
 function xLetterSubmit() {
-    if(confirm('익명 제보를 보내시겠습니까?')) {
-        //작성한 내용을 서버에 보내야 함
-        location.href="index.html";
+
+    const form = document.querySelector("#letterForm");
+    
+    if (letterField.value == "") {
+        letterField.className = "letterFieldError";
+        letterFieldErrorMessage.style.display = "block";
+        return false;
+    }
+    else {
+        if(confirm('익명 제보를 보내시겠습니까?')) {
+            form.submit();
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+function xLetterFieldNoError() {
+    if (letterField.className == "letterFieldError") {
+        letterField.className = "letterField";
+        letterFieldErrorMessage.style.display = "none";
     }
 }
